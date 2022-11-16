5c994badde72cfb8efb26b44aec875c34fd5945b
Herman Pelekh <herka03@gmail.com>
2022-11-16 11:41:53 +0200

    scss

--- src/app/components/login-dialog/login-dialog.component.ts
+++ src/app/components/login-dialog/login-dialog.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-login-dialog',
   templateUrl: './login-dialog.component.html',
-  styleUrls: ['./login-dialog.component.css']
+  styleUrls: ['./login-dialog.component.scss']
 })
 export class LoginDialogComponent implements OnInit {
 
